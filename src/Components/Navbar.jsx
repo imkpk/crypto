a4f8683f2813c584972d6b835aa0b7969f93e3ee
imkpk <imkpk@live.com>
2021-10-02 18:50:36 +0530

    added rapid-api and redux-toolkit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { Button, Menu, Typography, Avatar } from "antd";
-import { Link } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
 import {
   HomeOutlined,
   MoneyCollectOutlined,
   BulbOutlined,
   FundOutlined,
-  MenuOutlined
-} from "@ant-design/icons";
-import icon from "../images/crypto.png";
+  MenuOutlined,
+} from '@ant-design/icons';
+import icon from '../images/crypto.png';
 
 function Navbar() {
   return (
@@ -20,16 +21,16 @@ function Navbar() {
         </Typography.Title>
       </div>
       <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined/>}>
-          <Link to="/home">Home</Link>
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
         </Menu.Item>
-        <Menu.Item icon={<FundOutlined/>}>
+        <Menu.Item icon={<FundOutlined />}>
           <Link to="/cryptocurrencies">Cryptocurrencies</Link>
         </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined/>}>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
           <Link to="/exchanges">Exchanges</Link>
         </Menu.Item>
-        <Menu.Item icon={<BulbOutlined/>}>
+        <Menu.Item icon={<BulbOutlined />}>
           <Link to="/news">News</Link>
         </Menu.Item>
       </Menu>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "antd/dist/antd.css";
+import store from "./app/store";
+import { Provider } from "react-redux";
+
 ReactDOM.render(
   <Router>
-    <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
+    </Provider>
   </Router>,
   document.getElementById("root")
 );
