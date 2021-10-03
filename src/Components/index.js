389d401440f7f1b51712ff3e7d8f74de9cd4bd6c
imkpk <imkpk@live.com>
2021-10-04 05:20:25 +0530

    completed the crypto details

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,7 @@
-export {default as Navbar} from './Navbar'
-export {default as Cryptocurrencies} from './Cryptocurrencies'
-export {default as CryptoDetials} from './CryptoDetials'
-export {default as Homepage} from './Homepage'
-export {default as News} from './News'
-export {default as Exchanges}from './Exchanges'
+/* eslint-disable import/no-cycle */
+export { default as Homepage } from './Homepage';
+export { default as Navbar } from './Navbar';
+export { default as Cryptocurrencies } from './Cryptocurrencies';
+export { default as CryptoDetials } from './CryptoDetials';
+export { default as News } from './News';
+export { default as Exchanges } from './Exchanges';
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
-import "antd/dist/antd.css";
-import store from "./app/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import 'antd/dist/antd.css';
+import store from './app/store';
 
 ReactDOM.render(
   <Router>
@@ -13,5 +13,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
